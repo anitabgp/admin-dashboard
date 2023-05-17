a6fc1a5abbdc602252569c03721b86e572cbaaa6
anitabgp <anitabgp@gmail.com>
2023-05-17 12:08:26 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,84 +1,57 @@
 
-import { Component ,OnInit,ViewChild,OnDestroy} from '@angular/core';
-import {MatTableDataSource} from '@angular/material/table';
-import {DataSource, SelectionModel} from '@angular/cdk/collections';
-import { MatSort,Sort } from '@angular/material/sort';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { DataSource, SelectionModel } from '@angular/cdk/collections';
+import { MatSort, Sort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
 import { HttpClient } from '@angular/common/http';
 import { Subscription } from 'rxjs';
 
 
-export interface PeriodicElement {
-  position:number;
-  name: string;
-  email: string;
-  role: string;
-  actions:any;
-  
-  }
-   
-  export interface userFilter {
-    names:string;
-    value:string;
-   emails:string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-];
-
-  // {position: 1 , name: 'Aaron Miles', email: 'aaron@geektrust.in', role: 'Member',actions:''},
-  // {position: 2 , name: 'Aishwarya Naik', email: 'aishwarya@geektrust.in', role:'Member',actions:''},
-  // {position: 3, name: 'Arvind kumar', email: 'arvind@geektrust.in', role: 'Admin',actions:''},
-  // {position: 4 ,name: 'Catrina Binotoo', email: 'catrina@geektrust.in', role: 'Member',actions:''},
-  // {position: 5, name: 'chetan kumar', email: 'chetan@geektrust.in', role: 'Member',actions:''},
-  // {position: 6, name: 'Jim Mc Clain', email: 'jim@geektrust.in', role: 'Member',actions:''},
-  // {position: 7, name: 'Mahavir singh', email: 'mahavir@geektrust.in', role: 'Member',actions:''},
-  // {position: 8, name: 'Rahul Jain', email: 'rahul@geektrust.in', role: 'Member',actions:''},
-  // {position: 9 ,name: 'Rizan Khan', email: 'rizan@geektrust.in', role: 'Member',actions:''},
-  // {position: 10, name: 'Sarah Potter', email: 'sarah@geektrust.in', role: 'Member',actions:''}
-  
 
 
-  @Component({
+
+
+@Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
-   })
-  
-   export class AppComponent implements OnInit,OnDestroy{
-   
-    title = 'admin';
-   displayedColumns: string[] = ['select',  'name', 'email', 'role', 'actions'];
-   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-    
-    selection = new SelectionModel<PeriodicElement>(true, []);
-    userFilters: userFilter[]=[];
-     filterDictionary= new Map<string,string>();
-    subscription: any;
-    data:any;
-     constructor(private http: HttpClient) {
-      }
-     ngOnDestroy(): void {
-       if (this.subscription) {
-         this.subscription.unsubscribe();
+})
 
-    }
-  }
-  
-  
+export class AppComponent implements OnInit, OnDestroy {
+
+  title = 'admin';
+  displayedColumns: string[] = ['select', 'name', 'email', 'role', 'actions'];
+  dataSource = new MatTableDataSource<any>();
+  selection = new SelectionModel<any>(true, []);
+  userFilters: any[] = [];
+  filterDictionary: any;
+  subscription: any;
+  data: any[] = [];
+  @ViewChild(MatPaginator) paginator: any;
+
+  constructor(private http: HttpClient) { }
+
   /** Whether the number of selected elements matches the total number of rows. */
-  
+
   ngOnInit() {
-    this.userFilters.push({names:'name',value:'role',emails:'email'});
+    this.userFilters.push({ names: 'name', value: 'role', emails: 'email' });
     this.subscription = this.http.get("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json").subscribe({
-      next: (data) => this.data = data,
+      next: (data) => {
+        this.data = JSON.parse(JSON.stringify(data));
+        this.dataSource = new MatTableDataSource(this.data);
+        this.dataSource.paginator = this.paginator;
+
+      },
       error: (e) => console.error(e),
       complete: () => {
         console.log(this.data);
-        
-             }
+      }
     }
     )
+  }
+
+  ngAfterViewInit() {
   }
 
   isAllSelected() {
@@ -86,10 +59,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
     const numRows = this.dataSource.data.length;
     return numSelected === numRows;
   }
-  
-  ngAfterViewInit(): void {
 
-  }
   /** Selects all rows if they are not all selected; otherwise clear selection. */
   toggleAllRows() {
     if (this.isAllSelected()) {
@@ -99,20 +69,30 @@ const ELEMENT_DATA: PeriodicElement[] = [
 
     this.selection.select(...this.dataSource.data);
   }
-  
- 
- deletebut(){
-   window.alert("delbutton");
- }
- editbut(){
-   window.alert("editbutton");
- }
+
+
+  deletebut() {
+    window.alert("delbutton");
+  }
+  editbut() {
+    window.alert("editbutton");
+  }
 
   /** The label for the checkbox on the passed row */
-  checkboxLabel(row?:): string {
+  checkboxLabel(): string {
+    return "area-label";
+  }
+
+  checkboxLabel1(row: any): string {
     if (!row) {
       return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
     }
     return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+  }
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+
+    }
   }
 }
