dae8b2a990dee5505177182eaffbc1da40e1b751
anitabgp <anitabgp@gmail.com>
2023-06-13 19:21:59 +0530

    ch

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,7 +1,7 @@
  
 import { Component, OnInit,ViewEncapsulation,ViewChild} from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-import { DataSource, SelectionModel } from '@angular/cdk/collections';
+import { SelectionModel } from '@angular/cdk/collections';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Subscription } from 'rxjs';
 import { MatSort } from '@angular/material/sort';
@@ -53,16 +53,13 @@ import { MatPaginator } from '@angular/material/paginator';
     )
     
   }
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+  }
    applyFilter(event: Event) {
-       
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
-    console.log(this.dataSource.filter);
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-    }
-    
- 
+        
       }
-     
+      
+    }
